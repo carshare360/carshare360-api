e2ac835e6adb65fbf32f6b55393c41cc818d4a00
İsa Zobu <isa.zobu@getmidas.com>
2023-11-04 20:52:02 +0300

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    email: string;
+    @Column()
+    password: string;
+    
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+}
