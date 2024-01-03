8518859156c1639507a5a020e84609efba7218bf
isazobu <48869172+isazobu@users.noreply.github.com>
2024-01-04 01:28:28 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/v1/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
